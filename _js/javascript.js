64eba5742b6ac03de2788aa3d38ce582a9266f0a
Rodrigo-1980 <143399402+Rodrigo-1980@users.noreply.github.com>
2024-09-18 07:16:41 -0300

    atual_4.03

---
+++ _js/javascript.js
@@ -0,0 +1,4 @@
+document.getElementById('menu_responsivo').addEventListener('click', function() {
+    const menu_normal = document.getElementById('menu_normal');
+    menu_normal.classList.toggle('show');
+});
