f1632cf80ca5227ddfe6c3cd1f58b9c7a4f40c96
Rodrigo-1980 <143399402+Rodrigo-1980@users.noreply.github.com>
2024-10-29 07:14:38 -0300

    atual5,3

--- _js/javascript.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu_responsivo').addEventListener('click', function() {
-    const menu_normal = document.getElementById('menu_normal');
-    menu_normal.classList.toggle('show');
-});
